248d2881b847487ec5bee625e69f2dd8a122538a
Aditya Vaish <adityavaish846@gmail.com>
2025-08-03 19:33:39 +0530

    chakra UI added

--- backend/markit-minotaur.java
+++ backend/markit-minotaur.java
@@ -1,6 +1,6 @@
 //Markit-Minotaur
 
-class main {
+class markit-minotaur {
     public static void main(String[] args)  
     {     
         String message = "Goldman Sachs is a global investment banking, securities and investment management firm that provides a wide range of financial services to a substantial and diversified client base.";
