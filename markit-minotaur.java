32c9a9381447fe996a09f1faa9fa07ec22465916
Aditya Vaish <adityavaish846@gmail.com>
2025-08-04 13:49:30 +0530

    Software Architecture Changed

--- markit-minotaur.java
+++ markit-minotaur.java
@@ -1,9 +1,8 @@
 //Markit-Minotaur
 
-class markit-minotaur {
-    public static void main(String[] args)  
-    {     
-        String message = "Goldman Sachs is a global investment banking, securities and investment management firm that provides a wide range of financial services to a substantial and diversified client base.";
-        System.out.println(message);
+class markit_minotaur {
+    public static void main(String[] args){
+        
     }
+
 }
